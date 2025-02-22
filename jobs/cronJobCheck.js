d698d6244d1c9233bcc6b81c19e15b73a1005a11
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-02-21 22:16:32 -0500

    adding pm2 and other npm

--- jobs/cronJobCheck.js
+++ jobs/cronJobCheck.js
@@ -5,7 +5,11 @@ cron.schedule('*/15 * * * *', async () => {
     console.log(`[${new Date().toISOString()}] Lancement du job de scraping...`);
     try {
         const response = await axios.get('http://localhost:3000/api/health');
-        console.log(response.data);
+        console.log(response.data)
+        // Envoi du résultat du scraping au webhook Discord
+        await axios.post('https://discord.com/api/webhooks/1341564235469291521/QhGA40Vg_USmD6hlJQbjMF4JpoDrgdPfyGKIZIdssYLPt9gjjVUM4Q6R8BeaDz_nur9i', {
+            content: response.data   // Le contenu du message sera le résultat du scraping
+        });
     } catch (error) {
         console.error('Erreur lors du job de scraping :', error.message);
     }
