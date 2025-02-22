8ee68d0ed2e781717108ef6752e44a6b8794f7f8
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-02-21 21:50:45 -0500

    Adding setting bash

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ```bash
+npm install -g pm2
+
 npm install
 
 pm2 start ecosystem.config.js
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'scraper',
             script: './server.js',
-            instances: max, // Vous pouvez utiliser "max" pour exploiter tous les cœurs
+            instances: "max", // Vous pouvez utiliser "max" pour exploiter tous les cœurs
             exec_mode: 'cluster',
         },
         {
