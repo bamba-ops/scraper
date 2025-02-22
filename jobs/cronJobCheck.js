ddaa0ea17159ca37886b5042ef2585cf600d6923
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-02-21 21:52:23 -0500

    adding pm2 and other npm

--- jobs/cronJobCheck.js
+++ jobs/cronJobCheck.js
@@ -5,7 +5,7 @@ cron.schedule('*/2 * * * *', async () => {
     console.log(`[${new Date().toISOString()}] Lancement du job de scraping...`);
     try {
         const response = await axios.get('http://localhost:3000/api/health');
-        console.log(response);
+        console.log(response.data);
     } catch (error) {
         console.error('Erreur lors du job de scraping :', error.message);
     }
