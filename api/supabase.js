8c9cf5117f5384f007fb49e1541f1c257112aff1
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-02-27 18:17:32 -0500

    adding handle function to help parse

--- api/supabase.js
+++ api/supabase.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
     'https://pwwaxufdxuqfbdkduoll.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3d2F4dWZkeHVxZmJka2R1b2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2MDE5MDIsImV4cCI6MjA1NjE3NzkwMn0.9CMgP3QFsJESKlkHxJoU7y6dqUKZBOqpsbtSRvH3pQo'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3d2F4dWZkeHVxZmJka2R1b2xsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDYwMTkwMiwiZXhwIjoyMDU2MTc3OTAyfQ.a443UIXNWBWRooktCYOfwN3ii8wJ7b9o2Ne5wzCMymI'
 );
 
 module.exports = supabase
