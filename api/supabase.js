9c377ea5358da267597a092eba10241e66c6842a
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-03-01 19:37:50 -0500

    changing for .env file

--- api/supabase.js
+++ api/supabase.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-    'https://pwwaxufdxuqfbdkduoll.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3d2F4dWZkeHVxZmJka2R1b2xsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDYwMTkwMiwiZXhwIjoyMDU2MTc3OTAyfQ.a443UIXNWBWRooktCYOfwN3ii8wJ7b9o2Ne5wzCMymI'
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
 );
 
 module.exports = supabase
