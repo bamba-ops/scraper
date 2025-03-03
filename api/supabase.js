0510c47d6fc633e15d43b410200fbfca72f4e48e
wgharbi <wgharbi@dept-info.crosemont.quebec>
2025-03-03 14:13:55 -0500

    need to add product now, change the directory

--- api/supabase.js
+++ api/supabase.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY ? 'Exists' : 'Not Set');
+
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
